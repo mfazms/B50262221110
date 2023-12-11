f0c65a14b9c999d791ea8eb2f136a43cd7e1ba56
mfazms <fazlesidiki@gmail.com>
2023-12-11 12:18:52 +0700

    KODE SOAL AA Maaf pak, saya salah naruh lokasi gambarnya tadi jadi gamau muncul T_T
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   app/helpers.php
    	renamed:    resources/images/1.jpg -> public/images/1.png
    	new file:   public/images/2.png
    	new file:   public/images/3.png
    	new file:   public/images/4.png
    	new file:   public/images/5.png
    	new file:   resources/1.png
    	new file:   resources/2.png
    	new file:   resources/3.png
    	new file:   resources/4.png
    	new file:   resources/5.png
    	modified:   resources/images/.DS_Store
    	new file:   resources/images/1.png
    	new file:   resources/images/gambar.jpg

--- app/helpers.php
+++ app/helpers.php
@@ -3,11 +3,11 @@
 function convertEmoticonToImage($message)
 {
     $emoticons = [
-        ':))' => '<img src="' . url('images/1.png') . '" alt="">',
-        ':3'  => '<img src="' . url('images/2.png') . '" alt="">',
-        ':P'  => '<img src="' . url('images/3.png') . '" alt="">',
-        ':C'  => '<img src="' . url('images/4.png') . '" alt="">',
-        ';)'  => '<img src="' . url('images/5.png') . '" alt="">',
+        ':))' => '<img src="' . url('images/1.png') . '" alt="" style="width: 20px; height: 20px;">',
+        ':3'  => '<img src="' . url('images/2.png') . '" alt="" style="width: 20px; height: 20px;">',
+        ':P'  => '<img src="' . url('images/3.png') . '" alt="" style="width: 20px; height: 20px;">',
+        ':C'  => '<img src="' . url('images/4.png') . '" alt="" style="width: 20px; height: 20px;">',
+        ';)'  => '<img src="' . url('images/5.png') . '" alt="" style="width: 20px; height: 20px;">',
     ];
 
     foreach ($emoticons as $emoticon => $imageTag) {
